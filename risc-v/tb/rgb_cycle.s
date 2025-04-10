11d98e3ab599004e51272557ed49a3e91f9fe0c0
Daeyoung Kim <dkim@cellinobio.com>
2025-04-10 11:23:28 -0400

    Fix after simulation

--- risc-v/tb/rgb_cycle.s
+++ risc-v/tb/rgb_cycle.s
@@ -4,18 +4,17 @@ srli x3, x1, 16   # srli for green bits; x3 = 0x0000_FF00
 add x3, x1, x3    # add for green bits; x3 = 0xFF00_FF00
 srli x4, x1, 24   # srli for blue bits; x4 = 0x0000_00FF
 add x4, x1, x4    # add for blue bits; x4 = 0xFF00_00FF
-lui x5, 0x3FFFF   # lui for led address; x5 = 0x3FFF_F000
-sw x2, 0xFF0(x5)  # store red bits at address 0x3FFF_FFF0
-srli x6, x2, 12   # srli for counter; x6 = 0x000F_FFF0 ~0.7s delay including beq
+sw x2, -4(x0)     # store red bits at address 0xFFFF_FFFC
+srli x6, x2, 28   # srli for counter; x6 = 0x0000_000F (very small delay for simulation)
 addi x7, x7, 1    # increment counter x7
 blt x7, x6, -4    # branch back up to increment if less than 0x000F_FFF0
 addi x7, x0, 0    # reset counter x7 to 0
-sw x3, 0xFF0(x5)  # store green bits at address 0x3FFF_FFF0
+sw x3, -4(x0)     # store green bits at address 0xFFFF_FFFC
 addi x7, x7, 1    # increment counter x7
-blt x7, x6, -4    # branch back up to increment if less than 0x000F_FFF0
+blt x7, x6, -4    # branch back up to increment if less than 0x0000_000F
 addi x7, x0, 0    # reset counter x7 to 0
-sw x4, 0xFF0(x5)  # store blue bits at address 0x3FFF_FFF0
+sw x4, -4(x0)     # store blue bits at address 0xFFFF_FFFC
 addi x7, x7, 1    # increment counter x7
-blt x7, x6, -4    # branch back up to increment if less than 0x000F_FFF0
+blt x7, x6, -4    # branch back up to increment if less than 0x0000_000F
 addi x7, x0, 0    # reset counter x7 to 0
 jal x0, -52       # jump back up to storing red bits
