40b411f5cc70ec66fb24a2cdad0874458e7c7694
Daeyoung Kim <dkim@cellinobio.com>
2025-04-11 22:13:29 -0400

    Add all the tests

--- risc-v/tb/test_runner.py
+++ risc-v/tb/test_runner.py
@@ -3,6 +3,9 @@ from test_alu import test_alu
 from test_register_file import test_register_file
 from test_pc_selector import test_pc_selector
 from test_imm_extender import test_imm_extender
+from test_control import test_control
+from test_risc_v import test_risc_v
+from test_top import test_top
 
 
 def test_all():
@@ -11,3 +14,6 @@ def test_all():
     test_register_file()
     test_pc_selector()
     test_imm_extender()
+    test_control()
+    test_risc_v()
+    test_top()
