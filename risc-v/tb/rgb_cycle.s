e017a08b45bb89d23e48132dee05f133685c1b00
Daeyoung Kim <dkim@cellinobio.com>
2025-04-13 19:34:38 -0400

    Use active-low

--- risc-v/tb/rgb_cycle.s
+++ risc-v/tb/rgb_cycle.s
@@ -1,9 +1,10 @@
 lui x1, 0xFF000   # lui for led bits; x1 = 0xFF00_0000
-srai x2, x1, 8    # srai for red bits; x2 = 0xFFFF_0000
+srli x2, x1, 8    # srai for red bits; x2 = 0x00FF_0000
+xori x2, x2, -1    # xor for red bits; x2 = 0xFF00_FFFF
 srli x3, x1, 16   # srli for green bits; x3 = 0x0000_FF00
-add x3, x1, x3    # add for green bits; x3 = 0xFF00_FF00
+xori x3, x3, -1    # xor for green bits; x3 = 0xFFFF_00FF
 srli x4, x1, 24   # srli for blue bits; x4 = 0x0000_00FF
-add x4, x1, x4    # add for blue bits; x4 = 0xFF00_00FF
+xori x4, x4, -1    # xor for blue bits; x4 = 0xFFFF_FF00
 sw x2, -4(x0)     # store red bits at address 0xFFFF_FFFC
 srli x6, x2, 28   # srli for counter; x6 = 0x0000_000F (very small delay for simulation)
 addi x7, x7, 1    # increment counter x7
