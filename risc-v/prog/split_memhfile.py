3a74de8f2c8674f916b3aade2a816b0cb0e88391
Daeyoung Kim <dkim@cellinobio.com>
2025-04-14 17:18:42 -0400

    modify splitting to append 0s to 2048th line

--- risc-v/prog/split_memhfile.py
+++ risc-v/prog/split_memhfile.py
@@ -13,11 +13,22 @@ outfile1 = open(outfilename1, "w")
 outfile2 = open(outfilename2, "w")
 outfile3 = open(outfilename3, "w")
 
+n_lines = 0
 for line in infile:
+    if line.strip() == "":
+        continue
+    n_lines += 1
     outfile3.write(f"{line[0:2]}\n")
     outfile2.write(f"{line[2:4]}\n")
     outfile1.write(f"{line[4:6]}\n")
     outfile0.write(f"{line[6:8]}\n")
+
+if n_lines < 2048:
+    for i in range(2048 - n_lines):
+        outfile3.write("00\n")
+        outfile2.write("00\n")
+        outfile1.write("00\n")
+        outfile0.write("00\n")
 
 infile.close()
 outfile0.close()
