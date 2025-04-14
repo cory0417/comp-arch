0f31b413826c74e4218aa322a53493ef4418c3f9
Daeyoung Kim <dkim@cellinobio.com>
2025-04-13 22:26:14 -0400

    Add clock core

--- risc-v/rtl/top.sv
+++ risc-v/rtl/top.sv
@@ -1,19 +1,64 @@
 module top #(
-    parameter string INIT_FILE = ""
+    parameter string INIT_FILE = "@INIT_FILE@"  // Replaced by Makefile
 ) (
-    input  logic clk,
+`ifdef DEBUG
+    output logic _31b,
+    output logic _29b,
+    output logic _37a,
+    output logic _36b,
+    output logic _39a,
+`endif
     output logic LED,
     output logic RGB_R,
     output logic RGB_G,
     output logic RGB_B
 );
 
+`ifndef COCOTB_SIM  // Hard IP doesn't work in cocotb simulation
+  logic clk_12mhz, clk_48mhz;
+  SB_HFOSC #(
+      .CLKHF_DIV("0b00")  // 48 MHz; 0b00 = 48 MHz, 0b01 = 24 MHz, 0b10 = 12 MHz, 0b11 = 6 MHz
+  ) hfosc_inst (
+      .CLKHFEN(1'b1),
+      .CLKHFPU(1'b1),
+      .CLKHF  (clk_48mhz)
+  );
+  SB_PLL40_CORE #(
+      .FEEDBACK_PATH("SIMPLE"),
+      .DIVR         (4'd3),      // Divide by (3+1); 0,1,2,…,15
+      .DIVF         (7'd63),     // Multiply by (63+1); 0,1,..,63
+      .DIVQ         (3'd6),      // Divide by (2^6=64) => 12 MHz; 1,2,…,6
+      .FILTER_RANGE (3'd1)
+  ) pll_inst (
+      .REFERENCECLK(clk_48mhz),
+      .PLLOUTCORE(clk_12mhz),
+      .BYPASS(1'b0),
+      .RESETB(1'b1)
+  );
+`else
+  logic clk;
+`endif
+`ifdef DEBUG
+  // For debugging
+  assign _39a = clk_12mhz;
+  always_ff @(posedge clk_12mhz) begin
+    _31b <= LED;
+    _29b <= RGB_R;
+    _37a <= RGB_G;
+    _36b <= RGB_B;
+  end
+`endif
+
   logic [31:0] mem_ra, mem_wa, mem_rd, mem_wd;
   logic mem_wen;
   logic [2:0] mem_funct3;
 
   risc_v u_risc_v (
+`ifdef COCOTB_SIM
       .clk(clk),
+`else
+      .clk(clk_12mhz),
+`endif
       .mem_wen(mem_wen),
       .mem_ra(mem_ra),
       .mem_wa(mem_wa),
@@ -25,7 +70,11 @@ module top #(
   memory #(
       .INIT_FILE(INIT_FILE)
   ) u_memory (
+`ifdef COCOTB_SIM
       .clk(clk),
+`else
+      .clk(clk_12mhz),
+`endif
       .write_mem(mem_wen),
       .funct3(mem_funct3),
       .write_address(mem_wa),
